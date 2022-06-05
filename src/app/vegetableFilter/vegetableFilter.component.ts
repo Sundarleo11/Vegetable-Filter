2767e692a55b1f91294ca09a6c4bdd63a0b00727
sundar P <890536@cognizant.com>
2022-06-05 11:09:19 +0530

    firstcommit

--- src/app/vegetableFilter/vegetableFilter.component.ts
+++ src/app/vegetableFilter/vegetableFilter.component.ts
@@ -9,6 +9,30 @@ import {Component, Input, OnInit} from '@angular/core';
 export class VegetableFilter implements OnInit {
   @Input() vegetables: string[];
 
+  newVegetables:string[];
+  orgVeg:string[];
+  valid:boolean=true;
+
+  searchedVeg="";
+  onUpdate(event:Event){
+    this.searchedVeg=this.searchedVeg.trim();
+    this.vegetables=JSON.parse(
+      JSON.stringify(
+        this.newVegetables.filter((veg)=> veg.toLowerCase().startsWith(this.searchedVeg.toLowerCase().trim())
+        )
+      )
+    );
+
+    this.newVegetables=JSON.parse(JSON.stringify(this.orgVeg));
+    this.valid=true;
+    if(this.vegetables.length===0){
+      this.valid=false;
+    }
+  }
+
   ngOnInit() {
+    this.vegetables.forEach(veg=>veg=veg.trim());
+    this.newVegetables=JSON.parse(JSON.stringify(this.vegetables));
+    this.orgVeg=JSON.parse(JSON.stringify(this.vegetables));
   }
 }
